3bc2d9577a2e8bc4089e471afb1bb4e07d24523a
Rafael Scheidt <rafaelscheidt@Rafaels-MacBook-Air.local>
2024-04-01 08:29:20 -0300

    change principal class name

---
+++ src/test/java/br/com/agnes/ProjectArchetypeMvcApplicationTests.java
@@ -0,0 +1,15 @@
+package br.com.agnes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
+
+class ProjectArchetypeMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Test test test ...");
+		String test = "";
+		Assert.notNull(test, "mensagem...");
+	}
+
+}
---
+++ target/classes/db/migration/V1__init_data.sql
@@ -0,0 +1,15 @@
+--CREATE TABLE IF NOT EXISTS product (
+--    id INT PRIMARY KEY,
+--    name VARCHAR(255),
+--    date DATE NOT NULL,
+--    description VARCHAR(255)
+--);
+
+--INSERT INTO product(id, name, date, description)
+--VALUES (1, 'Product A', '2023-01-01', 'Product description A');
+
+--INSERT INTO product(id, name, date, description)
+--VALUES (2, 'Product B', '2023-01-02', 'Product description B');
+
+--INSERT INTO product(id, name, date, description)
+--VALUES (3, 'Product C', '2023-01-03', 'Product description C');
